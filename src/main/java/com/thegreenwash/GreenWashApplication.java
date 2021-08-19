b133d71c213c8d53cb28e9ac3ff182571fd1e934
Realeboga Sechele <sechele.rk@gmail.com>
2021-08-19 14:45:58 +0200

    Initial Commit

---
+++ src/main/java/com/thegreenwash/GreenWashApplication.java
@@ -0,0 +1,13 @@
+package com.thegreenwash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenWashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenWashApplication.class, args);
+	}
+
+}
